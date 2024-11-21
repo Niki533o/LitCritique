930a30a65e5ae6c32b5a34b006466a5f32ba92fd
Nikita <20463298@student.curtin.edu.au>
2024-11-22 03:05:55 +0530

    Updated Firebase configuration and added .env files

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDLVnPbAHUoocT5d5xNyB5uRm2lNKEnRuw",
-    authDomain: "litcritique-a8ac9.firebaseapp.com",
-    projectId: "litcritique-a8ac9",
-    storageBucket: "litcritique-a8ac9.firebasestorage.app",
-    messagingSenderId: "987609676210",
-    appId: "1:987609676210:web:74ba97dd90858ad9170cab",
-    measurementId: "G-1D59GH9PEC"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,   
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
